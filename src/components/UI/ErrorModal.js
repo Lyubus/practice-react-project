57f7f88d6c50f6887171ba383b0956d288d4a5a1
Oleg Lyubuskiy <oleg.lyubuskiy@jetsoftpro.com>
2021-06-29 19:01:55 +0300

    Add portal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,23 +1,37 @@
+import ReactDom from 'react-dom';
+
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
 
+const Backdrop = (props) => {
+    return (
+        <div onClick={props.onConfirm} className={classes.backdrop}></div>
+    )
+}
+
+const ModalOverlay = (props) => {
+    return (
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={classes.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={classes.actions}>
+                <Button onClick={props.onConfirm}>Ok</Button>
+            </footer>
+        </Card>
+    )
+}
+
 const ErrorModal = (props) => {
     return (
-        <div>
-            <div onClick={props.onConfirm} className={classes.backdrop}></div>
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onConfirm}>Ok</Button>
-                </footer>
-            </Card>
-        </div >
+        <>
+            {ReactDom.createPortal(<Backdrop onConfirm={props.onConfirm}></Backdrop>, document.getElementById('backdrop-root'))}
+            {ReactDom.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm}></ModalOverlay>, document.getElementById('overlay-root'))}
+        </>
     )
 }
 
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -45,7 +45,7 @@ const AddUser = (props) => {
     };
 
     return (
-        <div>
+        <>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}></ErrorModal>}
             <Card className={classes.input}>
                 <form onSubmit={addUserHabdler}>
@@ -58,7 +58,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </>
     );
 };
 
